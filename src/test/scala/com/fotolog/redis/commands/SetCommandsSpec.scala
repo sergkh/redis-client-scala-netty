62f4dd15686a445c58f9c3a952cad64435529510
faiaz <BlueDeepSpaces@gmail.com>
2016-07-22 18:11:05 +0300

    SetCommandsSpec

---
+++ src/test/scala/com/fotolog/redis/commands/SetCommandsSpec.scala
@@ -0,0 +1,11 @@
+package com.fotolog.redis.commands
+
+import com.fotolog.redis.TestClient
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by faiaz on 22.07.16.
+  */
+class SetCommandsSpec extends FlatSpec with Matchers with TestClient {
+
+}

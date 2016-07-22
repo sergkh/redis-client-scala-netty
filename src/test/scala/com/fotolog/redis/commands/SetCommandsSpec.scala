e5854db1e3ab2a9e225b5f240e21eef254302f17
faiaz <BlueDeepSpaces@gmail.com>
2016-07-22 21:35:42 +0300

    SetCommandsSpec 1.0

--- src/test/scala/com/fotolog/redis/commands/SetCommandsSpec.scala
+++ src/test/scala/com/fotolog/redis/commands/SetCommandsSpec.scala
@@ -7,5 +7,12 @@ import org.scalatest.{FlatSpec, Matchers}
   * Created by faiaz on 22.07.16.
   */
 class SetCommandsSpec extends FlatSpec with Matchers with TestClient {
+  val c = createClient()
+  c.flushall
 
+  "A sadd method" should "return Int result" in {
+    c.sadd("key", 4) shouldEqual 1
+    c.sadd("key", "Hello") shouldEqual 1
+    c.sadd("key", 4) shouldEqual 0
+  }
 }

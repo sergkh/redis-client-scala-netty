63fbf9b7a68becc0094c0afdf517371149e133de
faiaz <BlueDeepSpaces@gmail.com>
2016-07-24 13:28:01 +0300

    SetCommandsSpec 1.1.1(TestClient fix)

--- src/main/scala/com/fotolog/redis/RedisClient.scala
+++ src/main/scala/com/fotolog/redis/RedisClient.scala
@@ -18,7 +18,7 @@ object RedisClient {
   }
 
   @throws(classOf[AuthenticationException])
-  def apply(host: String = "",
+  def apply(host: String = "localhost",
             port: Int = 6379,
             password: Option[String] = None,
             timeout: Duration = DEFAULT_TIMEOUT) = {

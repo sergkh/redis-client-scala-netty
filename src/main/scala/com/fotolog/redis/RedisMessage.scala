6fdcf0f0314a6a2246551cc70f36c3bafbd22df3
yaroslav <yaroslav.derman@gmail.com>
2017-03-14 23:44:20 +0200

    modify redis response message

--- src/main/scala/com/fotolog/redis/RedisMessage.scala
+++ src/main/scala/com/fotolog/redis/RedisMessage.scala
@@ -1,24 +1,51 @@
 package com.fotolog.redis
 
+import com.fotolog.redis.connections.BulkDataResult
+import io.netty.util.CharsetUtil
+
 /**
   * @author Yaroslav Derman <yaroslav.derman@gmail.com>.
   *         created on 12.03.2017.
   */
 sealed trait RedisMessage
 
-case class StringRedisMessage(content: String) extends RedisMessage
-
-case object NullRedisMessage extends RedisMessage
-case class RawRedisMessage(content: Array[Byte]) extends RedisMessage
-case class IntRedisMessage(number: Int) extends RedisMessage
-case class ErrorRedisMessage(error: String) extends RedisMessage
-case class ArrayHeaderRedisMessage(length: Int) extends RedisMessage {
-  def isNull: Boolean = length == -1
+trait SimpleMessage extends RedisMessage {
+  def asOptBin: Option[Array[Byte]] = None
 }
 
-case class ArrayRedisMessage(children: List[RedisMessage], length: Int) extends RedisMessage {
-  def addMessage(msg: RedisMessage) = copy(children = this.children :+ msg)
-  def needPart = children.length < length
-  def needLastPart = length - children.length == 1
-  def isFull = children.length == length
+trait ComplexMessage extends RedisMessage
+
+case object NullRedisMessage extends SimpleMessage
+
+case object EmptyArrayRedisMessage extends SimpleMessage
+
+case class StringRedisMessage(content: String) extends SimpleMessage {
+  override def asOptBin: Option[Array[Byte]] = Option(content).map(_.getBytes(CharsetUtil.UTF_8))
 }
+
+case class RawRedisMessage(content: Array[Byte]) extends SimpleMessage {
+  override def toString: String = new String(content)
+
+  override def asOptBin: Option[Array[Byte]] = Some(content)
+}
+
+case class IntRedisMessage(number: Int) extends SimpleMessage {
+  override def asOptBin: Option[Array[Byte]] = Some(number.toString.getBytes)
+}
+
+case class ErrorRedisMessage(error: String) extends SimpleMessage {
+  override def asOptBin: Option[Array[Byte]] = Option(error).map(_.getBytes(CharsetUtil.UTF_8))
+}
+
+case class ArrayHeaderRedisMessage(length: Int) extends SimpleMessage {
+  override def asOptBin: Option[Array[Byte]] = None
+}
+
+case class ArrayRedisMessage(children: List[RedisMessage]) extends ComplexMessage {
+
+  //TODO: fixme
+  def asBulk: Seq[BulkDataResult] = children.flatMap {
+    case s: SimpleMessage => Seq(BulkDataResult(s.asOptBin))
+    case c: ArrayRedisMessage => c.asBulk
+  }
+} 
